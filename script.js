12e5afb3a0639a72be5de20635872251cb9a9b0c
Joel Straley <Joelstraley@gmail.com>
2020-11-25 00:43:12 -0500

    added pseduocode to begin planning

---
+++ script.js
@@ -0,0 +1,23 @@
+///set variables
+     //Possible Object with questions and choices 
+
+// Eventlistener for Start button 
+//Function for 1st question, answers 
+
+///While loop to create timer
+            // time subtracted up if wrong answer chosen 
+
+//Score board initiate to 0
+
+/// call on and create style for question
+
+///create style for buttons
+
+//Loop through questions
+
+//When all questions answere OR time = 0 game is over
+
+///end of game leades to High Score board where initals and score can be saved 
+
+
+//// Questions: 
