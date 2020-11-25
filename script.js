2a07f23b46a5a974d1724ac41343056b59942869
Joel Straley <Joelstraley@gmail.com>
2020-11-25 01:30:30 -0500

    questions added

--- script.js
+++ script.js
@@ -19,5 +19,28 @@
 
 ///end of game leades to High Score board where initals and score can be saved 
 
+/* 
+to get answers - li items:document.querySelector("li").getAttribute("right");
+document.querySelector("li").getAttribute("wrong");
+
+then change for next set of questions:document.querySelector("li").setAttribute("right","wrong")
+
+document.querySelector("li").style.background = "yellow" */
+
 
 //// Questions: 
+
+// Which logical operators are supported by JavaScript? 
+// and, or, not, all of the above
+
+// A piece of JavaScript programming wrapped in a value is called?
+// Get, Function, Log, Operator 
+
+//A Statement starting with "while" creates...
+//a skip, an object, an interval, a loop 
+
+//The first index of an array is...
+// One, Zero, Infinity, The Best
+
+//JavaScript is able to recall vaules by defining them through a 
+//Secret Lockbox, Variable, Brain, Dictionary
