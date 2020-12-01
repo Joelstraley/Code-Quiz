fec79b6a0f6e1366c37cad68bde9f9dad383dc2c
Joel Straley <Joelstraley@gmail.com>
2020-12-01 13:34:01 -0500

    built out questions, timer, scorekeeper and submit button

--- script.js
+++ script.js
@@ -3,82 +3,22 @@ var startBtn = document.getElementById("startBtn");
 var nextButton = document.getElementById("next-btn");
 var questionElement = document.getElementById("query");
 var answerButtonsElement = document.getElementById("answerButtons");
-
+var scoreHolderElement = document.getElementsById("scoreHolder");
+var scoreElement = document.getElementsById("score");
+var timeHolderElement = document.getElementById("timeHolder");
+var timerElement = document.getElementById("timer");
+var inputElement = document.getElementById("initials");
+var inputHolder 
 
 var shuffledQuestions, currentQuestionIndex
 
 var score = 0
+var timeLeft = 60;
+var running = false
 
-
-
-function startTimer() {
-var timeleft = 60;
-document.getElementById("timer").textContent = timeLeft;
-document.getElementById("timeHolder").setAttribute("style", "visiblity: visible;");
-var timer = setInterval(function(){
-timeleft--;
-if(timeleft ==== 0) 
-    clearInterval(timer);
-        gameOver(); /// or gameFail 
-}, 1000);
-} 
-
-
-function startQuiz() {
-        // hides the CODING QUIZ title and Start Button // 
-        title.setAttribute("style", "visibility:hidden;");
-        startBtn.setAttribute("style", "visibility: hidden;");
-        // makes the questions appear randomly //
-        shuffledQuestions = questions.sort(() => Math.random() - .5);
-        currentQuestionIndex = 0;
-        generateQuestion();
-        startTimer();
-}
-
-
-function generateQuestion() {
-        showQuestion(shuffledQuestions[currentQuestionIndex]);
-}
-
-function showQuestion(question) {
-        questionElement.innerText = question.question;
-
-}
-
-function resetState() {
-        nextButton.setAttribute("style", "visibility:hidden;");
-        while (answerButtonsElement.firstChild){
-          answersButtonElement.removeChild
-          (answerButtonsElement.firstChild)
-        }
-}
-
-function selectAnswer(e) {
-        var selectedButton = e.target;
-        correct = selectedButton.dataset.correct;
-        setStatusClass(document.body, correct)
-        Array.from(answerButtonsElement.children).forEach(button => {
-          setStatusClass(button, button.dataset.correct)          
-        })
-        nextButton.classList.remove('hide')
-}
-
-function setStatusClass(element, correct){
-        clearStatusClass(element)
-        if (correct) {
-          element.classList.add("correct")
-        } else {
-                element.classList.add("wrong")
-        }
-}
-
-function clearStatusClass(element){
-        element.classList.remove("correct");
-        element.classList.remove("wrong");
-}
 
 var questionSet = [
-        { 
+        {
           question: "Which logical operators are supported by JavaScript?",
           answers: [
                   {text: "and", correct: false },
@@ -124,6 +64,123 @@ var questionSet = [
                   ],
           },
   ]
+
+
+
+function startTimer() {
+        timerElement.textContent = timeLeft;
+        var timer = setInterval(function(){
+        timeLeft--;
+        timerElement.textContent = timeLeft;
+                if(timeLeft <= 0) {
+                gameOver(); /// or gameFail  
+                clearInterval(timer);      
+        } if (!running) {
+                clearInterval(timer);
+        }
+        }, 1000);
+}
+    
+
+function startQuiz() {
+        // hides the CODING QUIZ title,Start Button - Displays Timer // 
+        title.setAttribute("style", "visibility:hidden;");
+        startBtn.setAttribute("style", "visibility: hidden;");
+        
+        scoreElement.setAttribute("style", "display: block;");
+        timeHolderElement.setAttribute("style", "display: block;");
+        // makes the questions appear randomly //
+        shuffledQuestions = questionSet.sort(() => Math.random() - .5);
+        // set timer and score to 0 
+        running = true
+        setScore(score);
+
+        generateQuestion();
+        startTimer();
+}
+
+
+function generateQuestion() {
+        if (currentQuestionIndex == shuffledQuestions.length){
+                gameOver();
+        } else {
+        // randomizes question // 
+        showQuestion(shuffledQuestions[currentQuestionIndex]);
+        }
+}
+
+function gameOver() {
+        timeLeft = 0;
+        running = false;
+        answerButtonsElement.setAttribute("style", "display:none;");
+        questionElement.innerText = "Congratulations"
+        ///prompt for initials 
+}
+        
+function showQuestion(question) {
+                questionElBment.innerText = question.question;
+                var answerButtons = answerButtonsElement.getElementsByClassName("btn")
+                // loop through answers to pull and put in button elements // 
+                for (i = 0; i < answerbuttons.length; i++) {
+                        answerButtons[i].innerText = question["answers"][i].text;
+                        answerButtons[i].addEventListener("click", selectAnswer);
+                } // takes next question in questionSet Array 
+                currentQuestionIndex++;
+
+        }
+
+function resetState() {
+        nextButton.setAttribute("style", "visibility:hidden;");
+        while (answerButtonsElement.firstChild){
+          answersButtonElement.removeChild
+          (answerButtonsElement.firstChild)
+        }
+}
+
+function selectAnswer(event) {
+        var correct = checkAnswer(questionElement.innerText, event.srcElement.innerText)
+        if (correct {
+                //score go up
+         } else{
+                 // score go down + make time reduce 
+                 timeLeft = timeLeft - 10
+         } )
+}
+
+function checkAnswer(question, answer){
+        // loops through question set // 
+        for (i = 0; i < questionSet.length; i++) {
+                // check to see if Question is loop is same as asked//
+                if (questionSet[i].question === question) {
+                        // checks to make sure answers match //
+                        var answers = questionSet[i].answers;
+                        for (j = 0; j < answers.length; j++) {
+                                // if they match return if answer is correct or not  
+                                if (answer === answers[j].text){
+                                        return answers[j].correct;
+                                }
+                        }
+                  // if answers do not match return false       
+                } return false;
+        } // if questions do not match return false
+        return false; 
+}
+
+function setStatusClass(element, correct){
+        clearStatusClass(element)
+        if (correct) {
+          element.classList.add("correct")
+        } else {
+                element.classList.add("wrong")
+        }
+}
+
+function clearStatusClass(element){
+        element.classList.remove("correct");
+        element.classList.remove("wrong");
+}
+
+
 
                 /* function startTimer(duration, display) {
                         var timer = duration, minutes, seconds;
