fd7155e24464f4f5dbeea5d50c8a52a1c2b7678d
Joel Straley <Joelstraley@gmail.com>
2020-12-03 01:28:26 -0500

    fixed score counter, submit button saves intials to local storage

--- script.js
+++ script.js
@@ -3,29 +3,26 @@ var mainPage = document.getElementById("mainPage");
 var startBtn = document.getElementById("startBtn");
 var nextButton = document.getElementById("next-btn");
 var questionElement = document.getElementById("question");
-var answerButtonsElement = document.getElementById("answerButtons");
 var scoreHolderElement = document.getElementById("scoreHolder");
-var scoreElement = document.getElementById("score");
+var scoreElement = document.getElementById("scoreHolder");
+var answerButtonsElement = document.getElementById("answerButtons");
 var timeHolderElement = document.getElementById("timeHolder");
-var timerElement = document.getElementById("timer");
+var timerElement =  document.getElementById("timer");
 var inputElement = document.getElementById("initials");
 var inputHolder = document.getElementById("initialsHolder");
 var resultElement = document.getElementById("result");
-var submitBtn = document.getElementsById("submitIntBtn");
-
+var submitBtn = document.getElementById("submitIntBtn");
 
 var shuffledQuestions, currentQuestionIndex
 
-
-// save initials into local storage 
-// have that populate onto a seperate html page (OR delete all contect here.)
-
-var score = 0;
+var score = 0
 var timeLeft = 60;
 var running = false;
+var scoreArray = [];
+
 
 var questionSet = [
-        {
+        { 
           question: "Which logical operators are supported by JavaScript?",
           answers: [
                   {text: "and", correct: false },
@@ -72,181 +69,152 @@ var questionSet = [
           },
   ]
 
-
-
-function startTimer() {
+function startTimer() {  
+        // html score will show the amount of time left //
         timerElement.textContent = timeLeft;
         var timer = setInterval(function(){
-        timeLeft--;
-        timerElement.textContent = timeLeft;
+                timeLeft--;
+                timerElement.textContent = timeLeft;
                 if(timeLeft <= 0) {
-                gameOver(); /// or gameFail  
-                clearInterval(timer);      
-        } 
-        if (!running) {
-                clearInterval(timer);
-        }
-        }, 1000);
-}
+                    //Once score hits 0 or Game over function is ran clearing page //
+                        gameOver();
+                        clearInterval(timer);
+                }
+                if (!running) {
+                        clearInterval(timer);
+                }          
+                }, 1000);
+} 
 
 
 function startQuiz() {
-        // hides the CODING QUIZ title,Start Button - Displays Timer // 
+        // hides the CODING QUIZ title and Start Button // 
         title.setAttribute("style", "display: none;");
         startBtn.setAttribute("style", "display: none;");
-        
+        //displays Score, Timer, Answer buttons //
+
         scoreHolderElement.setAttribute("style", "display: block;");
         timeHolderElement.setAttribute("style", "display: block;");
         answerButtonsElement.setAttribute("style", "display: block;");
-        // set timer and score to 0 
+        //set that score to its initial value of 0
         running = true;
         setScore(score);
-        // makes the questions from array appear randomly //
+        // makes the questions appear randomly //
         shuffledQuestions = questionSet.sort(() => Math.random() - .5);
         currentQuestionIndex = 0;
         generateQuestion();
         startTimer();
 }
 
-
 function generateQuestion() {
-                // randomizes question, once array is looped through it goes to Game Over // 
-        if (currentQuestionIndex == shuffledQuestions.length){
+                    // randomizes question, once array is looped through it goes to Game Over // 
+        if (currentQuestionIndex == shuffledQuestions.length) {
                 gameOver();
         } else {
-        showQuestion(shuffledQuestions[currentQuestionIndex]);
+                showQuestion(shuffledQuestions[currentQuestionIndex]);
         }
 }
 
 function gameOver() {
-        // when game is over the timer is stopped
+            // when game is over the timer is stopped
         running = false;
-        // answer buttons are hidden
-        answerButtonsElement.setAttribute("style", "display: none;");
-        // and question text reads "Congratulations"
-        questionElement.innerText = "Congratulations"
+            // answer buttons are hidden
+        answerButtonsElement.setAttribute("style", "display:none");
+            // and question text reads "Congratulations"
+        questionElement.innerText = "Congratulations!";
+            // Submit initials input and button appear
         inputHolder.setAttribute("style", "display: flex;");
         submitBtn.addEventListener("click", saveInitials);
 }
-        
+
 function showQuestion(question) {
-                questionElement.innerText = question.question;
-                var answerButtons = answerButtonsElement.getElementsByClassName("btn")
-                // loop through answers to pull and put in button elements // 
-                for (i = 0; i < answerButtons.length; i++) {
-                        answerButtons[i].innerText = question["answers"][i].text;
-                        answerButtons[i].addEventListener("click", selectAnswer);
-                } // takes next question in questionSet Array 
-                currentQuestionIndex++;
-        }
-        
-        
+        questionElement.innerText = question.question;
+        var answerbuttons = answerButtonsElement.getElementsByClassName("btn")
+        // loop through answers to pull and put in button elements // 
+        for (i = 0; i < answerbuttons.length; i++) {
+                answerbuttons[i].innerText = question["answers"][i].text;
+                answerbuttons[i].addEventListener("click", selectAnswer)
+        } // takes next question in questionSet Array 
+        currentQuestionIndex++;
+}
+
+
 function selectAnswer(event) {
-        var correct = checkAnswer(questionElement.innerText, event.srcElement.innerText)
+        var correct = checkAnswer(questionElement.innerText, event.srcElement.innerText);
         if (correct) {
                 //if question is listed correct, score goes up
                 resultElement.innerText = "Correct";
                 score++;
                 setScore(score);
-                console.log(score);
-         } else{
+        } else {
                  // If incorrect time is reduced by 10
                  resultElement.innerText = "Wrong";
-                 timeLeft = timeLeft - 10
-         }  // back to generateQuestion function to populate next question
-         generateQuestion();
+                 timeLeft = timeLeft - 10;
+        }  // back to generateQuestion function to populate next question
+        generateQuestion();
 }
 
-function checkAnswer(question, answer){
-        // loops through question set // 
-        for (i = 0; i < questionSet.length; i++) {
-                // check to see if Question is loop is same as asked//
-                if (questionSet[i].question === question) {
-                        // checks to make sure answers match //
-                        var answers = questionSet[i].answers;
+function checkAnswer(question, answer) {
+          // loops through question set // 
+    for (i = 0; i < questionSet.length; i++) {
+         // check to see if Question is loop is same as asked// 
+        if (questionSet[i].question === question) {
+            // check to make sure answers match //
+                        var answers = questionSet[i].answers; 
                         for (j = 0; j < answers.length; j++) {
-                                // if they match return if answer is correct or not  
-                                if (answer === answers[j].text){
-                                        return answers[j].correct;
-                                }
-                        }
-                  // if answers do not match return false       
-                } return false;
+                    // if they match return if answer is correct or not  
+                            if (answer == answers[j].text) {
+                                       return answers[j].correct;
+                               }
+                        } // if answers do not match return false      
+                        return false;
+                } 
         } // if questions do not match return false
-        return false; 
+        return false;
 }
 
-function setScore(score){
-        // adds score value to score element
-      scoreElement.innerText = score;
+function setScore(score) {
+    //score is placed in score element after Score Holder
+        scoreElement.innerText = score;
 }
 
 function saveInitials(event){
-        var input = document.getElementById("initials");
-        localStorage.setItem("high scores", input.val())
+    var input = document.getElementById("initials");
+    // user's initials input and score and set to local storage //
+    scoreArray.push({"name": input.value, "score":score})
+    localStorage.setItem("high scores", scoreArray);
+    getInitials();
 }
 
-//Score board 
-// mainPage.innerHTML = "";// 
-//var storedValue = localStorage.getItem("high scores");
+function getInitials(){
+    mainPage.innerHTML = "";
+    localStorage.getItem("high scores");
+    list = document.createElement("ul");
+    list.setAttribute("id", "scoreList");
+    var i;
+    for (i = 0; i < scoreArray.length; i++) {
+      "<li>" + scoreArray[i] + "</li>";
+    } 
+}
+
+
+
+//log score to console
+//Print out as text on page 
+
+
+// looping through score array and then  append list item
+/* <ul>
+<li> name: {{name}} Score: {{score}} </li>
+</ul> // */
+
+
+
+
+// function playAgain button:
+ //Playagain button//
+
+/*  document.body.appendChild(board);
+ -playAgainbutton-eventlistener  */
 
 startBtn.addEventListener("click", startQuiz);
-
-
-
-                /* function startTimer(duration, display) {
-                        var timer = duration, minutes, seconds;
-                        setInterval(function () {
-                            seconds = parseInt(timer % 60, 10);
-                    
-                            minutes = minutes < 10 ? "0" + minutes : minutes;
-                             *//* seconds = seconds < 10 ? "0" + seconds : seconds;
-                    
-                            display.textContent = seconds;
-                    
-                            if (--timer < 0) {
-                                timer = duration;
-                            }
-                        }, 1000);
-                    }
-                    
-                    window.onload = function () {
-                        var fiveMinutes = 60 * 5,
-                            display = document.querySelector('#time');
-                        startTimer(fiveMinutes, display);
-                    }; */
-
-
-
-
-
-/* highScoresBtn.addEventListener("click", highScoresChart);
- */
-/* function highScoreButton() {
-} */
-
-
-
-
-
-///While loop to create timer
-            // time subtracted up if wrong answer chosen 
-
-//Score board initiate to 0
-
-/// call on and create style for question
-
-
-//Loop through questions
-
-//When all questions answere OR time = 0 game is over
-
-///end of game leades to High Score board where initals and score can be saved
-                ///High Score board will clear all HTML -- have link in corner 
-         /// document.getElementById(elementID).innerHTML = "";
-
-
-//to get answers - li items:document.querySelector("li").getAttribute("right");
-//document.querySelector("li").getAttribute("wrong");
-
-// then change for next set of questions:document.querySel//
